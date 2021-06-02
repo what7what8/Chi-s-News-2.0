a6ab8279008ff20fc050eb63ed62b94358a92a69
what7what8 <leungyinchi@gmail.com>
2021-06-02 19:03:40 +0800

    修復了點擊天氣出現崩潰。

--- app/src/main/java/com/chinews/xdapp/Listview.java
+++ app/src/main/java/com/chinews/xdapp/Listview.java
@@ -16,14 +16,16 @@ import java.util.List;
 import java.util.Objects;
 
 public class Listview extends AppCompatActivity {
-    private RecyclerView recycler_view;
     ArrayList<ArrayList<String>> arrayList = CheckJson.content;
+    private RecyclerView recycler_view;
+
     @Override
     public void onBackPressed() {
         Intent intent1 = new Intent(this, VipArea.class);
         startActivity(intent1);
         this.finish();
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,9 +39,9 @@ public class Listview extends AppCompatActivity {
         //}
         for (int i = 0; i < arrayList.size(); i++) {
             ArrayList<String> ij = arrayList.get(i);
-            if (ij.get(0).equals("null")) ij.set(0,"");
-            if (ij.get(1).equals("null") && !ij.get(0).equals("null")) ij.set(1,"沒有內容");
-            if (ij.get(1).equals("null")) ij.set(1,"");
+            if (ij.get(0).equals("null")) ij.set(0, "");
+            if (ij.get(1).equals("null") && !ij.get(0).equals("null")) ij.set(1, "沒有內容");
+            if (ij.get(1).equals("null")) ij.set(1, "");
         }
         List<String> title = new ArrayList<String>() {{
             add(getString(R.string.ag));
@@ -64,20 +66,23 @@ public class Listview extends AppCompatActivity {
         // 設置adapter給recycler_view
         recycler_view.setAdapter(adapter);
         SwipeRefreshLayout refreshLayout = findViewById(R.id.refru);
-        refreshLayout.setOnRefreshListener(() -> startActivity(new Intent(this,CheckJson.class).putExtra("json",2)));
+        refreshLayout.setOnRefreshListener(() -> startActivity(new Intent(this, CheckJson.class).putExtra("json", 2)));
     }
-    public void OnClick (View v){
+
+    public void OnClick(View v) {
         int position = recycler_view.getChildAdapterPosition(v);
         //Log.d("data", "onCreate: "+position);
-        boolean canc = Objects.equals(arrayList.get(position-1).get(3), "2");
-        if (position != 0 && canc) {
-            Intent intent = new Intent(this, ListviewInfo.class);
-            intent.putExtra("pos", position-1);
-            startActivity(intent);
-        } else if (position == 0) {
+        if (position <= 0) {
             Intent intent = new Intent(this, CheckJson.class);
             intent.putExtra("json", 1);
             startActivity(intent);
+        }else {
+            boolean canc = Objects.equals(arrayList.get(position - 1).get(3), "2");
+            if (canc) {
+                Intent intent = new Intent(this, ListviewInfo.class);
+                intent.putExtra("pos", position - 1);
+                startActivity(intent);
+            }
         }
     }
 }
